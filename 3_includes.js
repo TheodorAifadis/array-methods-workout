043bb2fc2cbd574695c11eac59d667e8351f1c5c
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2020-12-07 16:07:24 +0100

    includes

---
+++ 3_includes.js
@@ -0,0 +1,8 @@
+// Includes
+// Metoden talar om ifall en array innehåller ett visst element
+// Sedan returneras true eller false beroende på om den innehåller elementet eller inte
+
+const sports = ['fotboll', 'basket', 'bandy', 'hockey', 'handboll']
+
+console.log(sports.includes('basket')) // borde ge true
+console.log(sports.includes('volleyboll')) // borde ge false
