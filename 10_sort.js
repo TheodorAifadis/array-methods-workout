731c1260edb383f656fad016b99ce13e4a1c8ba3
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2020-12-13 14:48:34 +0100

    sort

---
+++ 10_sort.js
@@ -0,0 +1,6 @@
+// sort, en metod som sorterar en array och ordnar om, byter plats på elementen i arrayen
+// så att de hamnar sorterat i rätt ordning
+// metoden kan sortera element utifrån olika aspekter som exempelvis bokstavsordning
+const legends = ['pele', 'maradona', 'cantona', 'best', 'zidane', 'eusebio']
+legends.sort()
+console.log(legends)
