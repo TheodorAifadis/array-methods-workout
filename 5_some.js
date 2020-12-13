7a287488ffceae57e2b2f9141ba88196e9ad1f58
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2020-12-13 14:20:33 +0100

    some

---
+++ 5_some.js
@@ -0,0 +1,5 @@
+// some, denna metod checkar så att minst ett element i en array uppfyller funktionen
+// den returner alltså sedan true eller false beroende på om det uppfyller funktionen eller ej
+const players = ['eric', 'pep', 'jurgen', 'lionel', 'diego']
+console.log(players.some(x => x.length === 4)) //true
+console.log(players.some(x => x.length === 5)) //false
