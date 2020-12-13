01e857134fffc5f0977544716176c27d6de174a7
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2020-12-13 14:55:37 +0100

    fill

---
+++ 11_fill.js
@@ -0,0 +1,4 @@
+// fill är en metod som ändrar element i en array till ett värde
+// metoden byter ut följande element till ett annat ord
+const cars = ['lamborghini', 'bmw', 'audi', 'volkswagen', 'porsche', 'saab', 'renault', 'ferrari']
+console.log(cars.fill('VOLVO', 1, 7)) // det här borde byta ut element 1-7 till VOLVO
